2d1f8cc0549a961c68f5826e0e216b115e16bfc9
Mibble <mibble@outlook.com.au>
2026-05-31 11:54:58 +0930

    Fix special whitespace characters remaining after emoji strip.

--- src/strip-emoji.js
+++ src/strip-emoji.js
@@ -2,6 +2,6 @@ export function strip(str) {
     // Source - https://stackoverflow.com/a/41543705
     // Posted by jony89, modified by community. See post 'Timeline' for change history
     // Retrieved 2026-05-30, License - CC BY-SA 4.0
-    return str.replace(/\p{Emoji_Presentation}/gu, '');
+    return str.replace(/\p{Emoji}/gu, '').replace(/\p{Nonspacing_Mark}/gu, '');
 
 }
