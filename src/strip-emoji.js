9c758aa50d1874b990b37c67b4011e0517ff6ef4
Mibble <mibble@outlook.com.au>
2026-06-21 14:44:15 +0930

    Strip joining control characters

--- src/strip-emoji.js
+++ src/strip-emoji.js
@@ -2,6 +2,8 @@ export function strip(str) {
     // Source - https://stackoverflow.com/a/41543705
     // Posted by jony89, modified by community. See post 'Timeline' for change history
     // Retrieved 2026-05-30, License - CC BY-SA 4.0
-    return str.replace(/\p{Emoji}/gu, '').replace(/\p{Nonspacing_Mark}/gu, '');
-
+    return str
+        .replace(/\p{Emoji}/gu, '')
+        .replace(/\p{Nonspacing_Mark}/gu, '')
+        .replace(/\p{Join_Control}/gu, '');
 }
